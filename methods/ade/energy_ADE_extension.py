048038fc1cb5140c33fc588bf8414def90e02dd4
ElTure97 <salvatore.finocchiaro_97@outlook.com>
2023-07-24 20:52:53 +0200

    new commit

--- methods/ade/energy_ADE_extension.py
+++ methods/ade/energy_ADE_extension.py
@@ -18,23 +18,35 @@ class EnergyADE:
             else:
                 constr_weight = "heavy"
 
+            bld_type = bld_elem[self.headers[6]]
+            if bld_type == "AB":
+                bld_type_str = "apartmentBlock"
+            elif bld_type == "SFH":
+                bld_type_str = "singleFamilyHouse"
+            elif bld_type == "MFH":
+                bld_type_str = "multiFamilyHouse"
+            elif bld_type_str == "TH":
+                bld_type_str = "terracedHouse"
+            else:
+                bld_type_str = bld_type
+
             energy = {
-            "+energy-buildingType": bld_elem[self.headers[6]],
+            "+energy-buildingType": bld_type_str,
             "+energy-constructionWeight": constr_weight,
             "+energy-volume": [{
                 "energy-type": "grossVolume",
                 "energy-value":  round((float(bld_elem[self.headers[5]]) * float(bld_elem[self.headers[0]])), 2),
-                "uom": "m3"
+                "energy-uom": "m3"
             }],
             "+energy-floorArea": [{
                 "energy-type": "grossFloorArea",
                 "energy-value": float(bld_elem[self.headers[5]]),
-                "uom": "m2"
+                "energy-uom": "m2"
             }],
             "+energy-heightAboveGround": [{
                 "energy-heightReference": "generalEave",
                 "energy-value": float(bld_elem[self.headers[0]]),
-                "uom": "m"
+                "energy-uom": "m"
             }],
             "+energy-weatherData": [{
                 "energy-weatherElement": "airTemperature",
